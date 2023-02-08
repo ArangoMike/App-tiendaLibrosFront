a56831150841727e9c0cdb140e194f9435b1a460
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-02-08 15:38:49 -0500

    creacion proyecto y empaquetamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LobbyComponent } from './paginas/lobby/lobby.component';
+import { TiendaComponent } from './paginas/tienda/tienda.component';
+import { CarritoComponent } from './paginas/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LobbyComponent,
+    TiendaComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
